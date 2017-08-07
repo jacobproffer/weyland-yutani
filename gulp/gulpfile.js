bbd7ddaae19cc7953cfc3c5009aee93552bd3f96
jacobproffer <jacobhardingproffer@gmail.com>
2017-08-06 21:31:32 -0400

    Refactor gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -11,33 +11,33 @@ var gulp        = require('gulp'),
     browserSync = require('browser-sync').create();
 
 var scripts = [
-  'assets/js/lib/jquery-3.2.1.min.js',
-  'assets/js/lib/headroom/headroom.min.js',
-  'assets/js/lib/headroom/jQuery.headroom.js',
-  'assets/js/app.js'
+  '../assets/js/lib/jquery-3.2.1.min.js',
+  '../assets/js/lib/headroom/headroom.min.js',
+  '../assets/js/lib/headroom/jQuery.headroom.js',
+  '../assets/js/app.js'
 ];
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass', 'js'], function() {
 
     browserSync.init({
-        server: './',
+        server: '../',
         browser: "google chrome"
     });
 
-    gulp.watch('assets/scss/**/*.scss', ['sass']);
-    gulp.watch('assets/js/**/*.js', ['js']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('../assets/scss/**/*.scss', ['sass']);
+    gulp.watch('../assets/js/**/*.js', ['js']);
+    gulp.watch('../*.html').on('change', browserSync.reload);
 });
 
 // Configure CSS tasks.
 gulp.task('sass', function () {
-  return gulp.src('assets/scss/**/*.scss')
+  return gulp.src('../assets/scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
     .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest('../dist/css'))
     .pipe(browserSync.stream());
 });
 
@@ -47,21 +47,23 @@ gulp.task('js', function() {
     .pipe(uglify())
     .pipe(concat('app.js'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('../dist/js'))
     .pipe(browserSync.stream());
 });
 
 // Configure image stuff.
 gulp.task('images', function () {
-  return gulp.src('assets/img/**/*.+(png|jpg|gif|svg)')
+  return gulp.src('../assets/img/**/*.+(png|jpg|gif|svg)')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+    .pipe(gulp.dest('../dist/img'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
-  gulp.watch('dist/scss/**/*.scss', ['sass']);
-  gulp.watch('dist/js/**/*.js', ['js']);
-  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('../assets/img/**/*.+(png|jpg|gif|svg)');
+  gulp.watch('../dist/scss/**/*.scss', ['sass']);
+  gulp.watch('../dist/js/**/*.js', ['js']);
+  gulp.watch('../*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['sass', 'js', 'images', 'serve']);
